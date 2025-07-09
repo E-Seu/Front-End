5d89593c12b123c7caae56be46bb4a4e81d5875c
Gab <91504607+gabryellaro@users.noreply.github.com>
2025-07-08 22:06:26 -0300

    Alterando icon de coracao

--- src/assets/icons/coracaoIcon.js
+++ src/assets/icons/coracaoIcon.js
@@ -5,47 +5,24 @@ const CoracaoIcon = ({ isActive = false, width = 50, height = 50 }) => {
   if (isActive) {
     // Versão ativa (preenchida)
     return (
-      <Svg 
+   <Svg 
         width={width} 
         height={height} 
-        viewBox="0 0 50 50" 
+        viewBox="0 0 50 49" 
         fill="none"
       >
-        <G clipPath="url(#clip0_500_1043)" filter="url(#filter0_d_500_1043)">
-          <G filter="url(#filter1_d_500_1043)">
-            <Path 
-              d="M4.58331 18.6546C4.58331 28.5832 12.7908 33.8732 18.7974 38.6098C20.9166 40.2799 22.9583 41.8541 25 41.8541C27.0416 41.8541 29.0833 40.282 31.2026 38.6078C37.2112 33.8752 45.4166 28.5832 45.4166 18.6566C45.4166 8.73005 34.1875 1.68426 25 11.2311C15.8125 1.68426 4.58331 8.72597 4.58331 18.6546Z" 
-              fill="#D00000" 
-              fillOpacity="0.96" 
-              shapeRendering="crispEdges"
-            />
-          </G>
-        </G>
-        <Defs>
-          <Filter id="filter0_d_500_1043" x="-3.5" y="0" width="57" height="57" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-            <FeFlood floodOpacity="0" result="BackgroundImageFix"/>
-            <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-            <FeOffset dy="4"/>
-            <FeGaussianBlur stdDeviation="2"/>
-            <FeComposite in2="hardAlpha" operator="out"/>
-            <FeColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-            <FeBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_500_1043"/>
-            <FeBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_500_1043" result="shape"/>
-          </Filter>
-          <Filter id="filter1_d_500_1043" x="4.58331" y="6.7063" width="44.8333" height="39.1477" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-            <FeFlood floodOpacity="0" result="BackgroundImageFix"/>
-            <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-            <FeOffset dx="2" dy="2"/>
-            <FeGaussianBlur stdDeviation="1"/>
-            <FeComposite in2="hardAlpha" operator="out"/>
-            <FeColorMatrix type="matrix" values="0 0 0 0 0.6875 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-            <FeBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_500_1043"/>
-            <FeBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_500_1043" result="shape"/>
-          </Filter>
-          <ClipPath id="clip0_500_1043">
-            <Rect width="49" height="49" fill="white" transform="translate(0.5)"/>
-          </ClipPath>
-        </Defs>
+        {/* Sombra - mesmo path deslocado */}
+       <Path 
+          d="M4.08331 20.6546C4.08331 30.5832 12.2908 35.8732 18.2974 40.6098C20.4166 42.2799 22.4583 43.8541 24.5 43.8541C26.5416 43.8541 28.5833 42.282 30.7026 40.6078C36.7112 35.8752 44.9166 30.5832 44.9166 20.6566C44.9166 10.73 33.6875 3.68426 24.5 13.2311C15.3125 3.68426 4.08331 10.726 4.08331 20.6546Z" 
+          fill="#000000" 
+          fillOpacity="0.3"
+        />
+        {/* Coração principal */}
+        <Path 
+          d="M4.08331 18.6546C4.08331 28.5832 12.2908 33.8732 18.2974 38.6098C20.4166 40.2799 22.4583 41.8541 24.5 41.8541C26.5416 41.8541 28.5833 40.282 30.7026 38.6078C36.7112 33.8752 44.9166 28.5832 44.9166 18.6566C44.9166 8.73005 33.6875 1.68426 24.5 11.2311C15.3125 1.68426 4.08331 8.72597 4.08331 18.6546Z" 
+          fill="#D00000" 
+          fillOpacity="0.96"
+        />
       </Svg>
     );
   } else {
